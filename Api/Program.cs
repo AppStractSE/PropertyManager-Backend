093667bd11792d5b2a62c7acfb0b2b017b56610e
4l3x <alexander.auermaa@gmail.com>
2022-12-20 20:01:37 +0100

    added packages

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,8 +1,21 @@
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Mapster;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Setup mediatR
+
+var config = TypeAdapterConfig.GlobalSettings;
+config.Scan(typeof(this).Assembly);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
