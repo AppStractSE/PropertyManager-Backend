18a25bb874d69043bfafe32af68acc1a0145d430
Lucas Benjaminson <lucas_benjaminson@hotmail.com>
2023-01-03 15:17:32 +0100

    Post User controller

--- Api/Controllers/v1/UserController.cs
+++ Api/Controllers/v1/UserController.cs
@@ -3,6 +3,8 @@ using MapsterMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Domain.Domain;
+using Api.Dto.Request.User.v1;
+using Domain.Features.Commands.User;
 
 namespace Api.Controllers.v1;
 
@@ -23,6 +25,13 @@ public class UserController : ControllerBase
     public async Task<ActionResult<IList<User>>> GetAllUsers()
     {
         var result = await _mediator.Send(new GetAllUsersQuery());
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<User>> PostChoreCommentAsync(PostUserRequestDto request)
+    {
+        var result = await _mediator.Send(_mapper.Map<PostUserRequestDto, AddUserCommand>(request));
         return Ok(result);
     }
 }
