b05b15ca959f431366ac82f50b0e73e423d82a0f
Zaai90 <erik.grune@gmail.com>
2023-01-17 19:59:23 +0100

    Add loginlogic for frontend

--- Api/Controllers/v1/AuthenticateController.cs
+++ Api/Controllers/v1/AuthenticateController.cs
@@ -2,6 +2,7 @@
 using Domain.Features.Commands.Authentication.Create;
 using MapsterMapper;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
@@ -65,9 +66,11 @@ public class AuthenticateController : ControllerBase
         return Ok(new AuthResponse { Status = "Success", Message = "User created successfully!" });
     }
 
+    [Authorize(Roles = "Admin")]
     [HttpPost]
     [Route("register-admin")]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> RegisterAdmin([FromBody] RegisterModel model)
     {
