d75d1314bb4e2ef15080f3a95c04e4a6defb9aa1
Lucas Benjaminson <lucas_benjaminson@hotmail.com>
2023-02-08 20:14:22 +0100

    changed chorestatus request dto

--- Api/Dto/Requests/ChoreStatus/v1/PostChoreStatusRequestDto.cs
+++ Api/Dto/Requests/ChoreStatus/v1/PostChoreStatusRequestDto.cs
@@ -3,7 +3,5 @@ namespace Api.Dto.Request.ChoreStatus.v1;
 public class PostChoreStatusRequestDto
 {
     public string? CustomerChoreId { get; set; }
-    public DateTime StartDate { get; set; } = DateTime.Now;
-    public DateTime CompletedDate { get; set; } = DateTime.Now;
     public string? DoneBy { get; set; }
 }
