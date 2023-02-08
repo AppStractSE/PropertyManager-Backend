7b8d8bb8e42bc3427f8ae38920d47d3d8be598c8
Lucas Benjaminson <lucas_benjaminson@hotmail.com>
2023-02-08 19:23:08 +0100

    Post chore status dto removed id

--- Api/Dto/Requests/ChoreStatus/v1/PostChoreStatusRequestDto.cs
+++ Api/Dto/Requests/ChoreStatus/v1/PostChoreStatusRequestDto.cs
@@ -2,7 +2,6 @@ namespace Api.Dto.Request.ChoreStatus.v1;
 
 public class PostChoreStatusRequestDto
 {
-    public Guid Id { get; set; }
     public string? CustomerChoreId { get; set; }
     public DateTime StartDate { get; set; } = DateTime.Now;
     public DateTime CompletedDate { get; set; } = DateTime.Now;
