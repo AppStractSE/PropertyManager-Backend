4ff8c7095e1045b1cf3728c00fd97e2fc5cfc2cb
4l3x <alexander.auermaa@gmail.com>
2023-03-18 01:19:45 +0100

    cache now removed on put

--- Core/Features/Commands/Customer/Update/UpdateCustomerCommandHandler.cs
+++ Core/Features/Commands/Customer/Update/UpdateCustomerCommandHandler.cs
@@ -9,14 +9,19 @@ public class UpdateCustomerCommandHandler : IRequestHandler<UpdateCustomerComman
 {
     private readonly ICustomerRepository _repo;
     private readonly IMapper _mapper;
-    public UpdateCustomerCommandHandler(ICustomerRepository repo, IMapper mapper)
+    private readonly ICache _cache;
+    
+    public UpdateCustomerCommandHandler(ICustomerRepository repo, IMapper mapper, ICache cache)
     {
         _repo = repo;
         _mapper = mapper;
+        _cache = cache;
     }
     public async Task<Domain.Customer> Handle(UpdateCustomerCommand request, CancellationToken cancellationToken)
     {
         var response = await _repo.UpdateAsync(_mapper.Map<Repository.Entities.Customer>(request));
+        await _cache.RemoveAsync("Customers:");
+        await _cache.RemoveAsync($"Customer:{request.Id}");
         return _mapper.Map<Domain.Customer>(response);
     }
 }
