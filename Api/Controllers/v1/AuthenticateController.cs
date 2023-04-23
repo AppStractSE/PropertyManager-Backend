884e71712ab16be76af5ac473cb7d9d3b9deb003
Erik <erik.grune@gmail.com>
2023-04-23 18:48:34 +0200

    add authorize for validation endpoint

--- Api/Controllers/v1/AuthenticateController.cs
+++ Api/Controllers/v1/AuthenticateController.cs
@@ -68,6 +68,7 @@ public class AuthenticateController : ControllerBase
         }
     }
 
+    [Authorize]
     [HttpGet]
     [Route("validation")]
     [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(AuthUser))]
