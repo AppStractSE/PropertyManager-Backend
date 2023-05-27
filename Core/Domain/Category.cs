e20fc52839e60122e4facc43939a3eec6e7d2b17
Erik <erik.grune@gmail.com>
2023-05-27 03:23:26 +0200

    Fixes for new category system

--- Core/Domain/Category.cs
+++ Core/Domain/Category.cs
@@ -3,6 +3,8 @@ namespace Core.Domain;
 public class Category
 {
     public Guid Id { get; set; }
+    public Guid ParentId { get; set; }
+    public bool IsParent { get; set; }
     public string Title { get; set; }
     public string Reference { get; set; }
     public IList<Category> SubCategories { get; set; }
--- Core/Features/Queries/Category/GetAllCategoriesQueryHandler.cs
+++ Core/Features/Queries/Category/GetAllCategoriesQueryHandler.cs
@@ -16,10 +16,13 @@ public class GetAllCategoriesQueryHandler : IRequestHandler<GetAllCategoriesQuer
     }
     public async Task<IList<Category>> Handle(GetAllCategoriesQuery request, CancellationToken cancellationToken)
     {
-        var categories = _mapper.Map<IList<Category>>(await _repo.GetAllAsync());
+        var categoryEntities = await _repo.GetAllAsync();
+        var categories = _mapper.Map<IList<Category>>(categoryEntities);
         foreach (var category in categories)
         {
-            category.SubCategories = _mapper.Map<IList<Category>>(await _repo.GetQuery(x => x.ParentId == category.Id));
+            var categoryChildren = categoryEntities.Where(x => x.ParentId == category.Id).ToList();
+            category.IsParent = category.ParentId == default || categoryChildren.Any();
+            category.SubCategories = _mapper.Map<IList<Category>>(categoryChildren);
         }
         return categories;
     }
