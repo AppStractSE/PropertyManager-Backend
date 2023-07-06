9e92c90a6d7e52c57d930c4033e971a5ae140b0e
Erik <erik.grune@gmail.com>
2023-07-07 00:46:16 +0200

    add logic to generate report object

--- Core/Features/Queries/Report/GetReportQueryHandler.cs
+++ Core/Features/Queries/Report/GetReportQueryHandler.cs
@@ -1,36 +1,46 @@
-﻿using Core.Repository.Interfaces;
+﻿using Core.Domain.Report;
+using Core.Features.Queries.CustomerChores;
+using Core.Features.Queries.Customers;
+using Core.Repository.Interfaces;
 using MapsterMapper;
 using MediatR;
-using Core.Domain.Report;
-using System.Runtime.CompilerServices;
-using Core.Features.Queries.Customers;
-using Core.Features.Queries.CustomerChores;
 
 namespace Core.Features.Queries.Report;
 
-    public class GetReportQueryHandler : IRequestHandler<GetReportQuery, byte[]>
-    {
-        private readonly IMapper _mapper;
-        private readonly IMediator _mediator;
+public class GetReportQueryHandler : IRequestHandler<GetReportQuery, byte[]>
+{
+    private readonly IMapper _mapper;
+    private readonly IMediator _mediator;
     private readonly IArchivedChoreStatusRepository _archRepo;
 
-        public GetReportQueryHandler(IMapper mapper, IMediator mediator, IArchivedChoreStatusRepository archRepo)
-        {
-            _archRepo = archRepo;
-            _mediator = mediator;
-            _mapper = mapper;
-        }
+    public GetReportQueryHandler(IMapper mapper, IMediator mediator, IArchivedChoreStatusRepository archRepo)
+    {
+        _archRepo = archRepo;
+        _mediator = mediator;
+        _mapper = mapper;
+    }
 
-        public async Task<byte[]> Handle(GetReportQuery request, CancellationToken cancellationToken)
-        {
+    public async Task<byte[]> Handle(GetReportQuery request, CancellationToken cancellationToken)
+    {
         var reportObject = await GenerateReportObjectAsync(request.CustomerId, cancellationToken);
 
 
+        // Add logic to generate report here
+
+        // TODO:
+        // 1. Create a new instance of the report (X)
+        // 2. Generate the report object (X)
+        // 3. Generate the report in xls <---
+        // 4. Return the report as a byte array (/)
+        // 5. Add a new endpoint in the controller to return the report as a file 
+        // Done!
+
         return await Task.FromResult(new byte[0]);
-        }
+    }
 
     private async Task<ReportObject> GenerateReportObjectAsync(string customerId, CancellationToken cancellationToken = default)
     {
+        var dateNow = DateTime.Now;
         var choreRows = new List<ChoreRow>();
         var customerInfo = _mapper.Map<CustomerInfo>(await _mediator.Send(new GetCustomerByIdQuery { Id = customerId }));
         var issuerInfo = new IssuerInfo
@@ -40,57 +50,59 @@ namespace Core.Features.Queries.Report;
             Address = "Property Manager Street 1",
             Email = "Property Manager City",
             PhoneNumber = "12345"
-           };
+        };
+
+
         var customerChores = await _mediator.Send(new GetCustomerChoresByCustomerIdQuery { Id = customerId });
-        foreach(var customerChore in customerChores)
+
+        foreach (var customerChore in customerChores)
         {
-            var allAchivedChores = _archRepo.GetQuery(x => x.CustomerChoreId == customerChore.Id.ToString());
-            var achivedChores = customerChores.Where(x => x.ChoreId == customerChore.ChoreId && x.).ToList();
+            var allAchivedChores = await _archRepo.GetQuery(x => x.CustomerChoreId == customerChore.Id.ToString());
+            var monthResult = new List<MonthResult>();
+            for (var i = 0; i < dateNow.Month; i++)
+            {
+                var monthNr = i + 1;
+                var thisMonthResult = new MonthResult
+                {
+                    MonthNr = monthNr,
+                    Progress = "Not Done"
+                };
+
+                var achivedChore = allAchivedChores.Select(x => x.CompletedDate.Month == monthNr && x.CompletedDate.Year == dateNow.Year);
+                var progress = (decimal)(achivedChore.Count() / customerChore.Frequency);
+
+                if (progress > 0)
+                {
+                    thisMonthResult.Progress = "0";
+                }
+
+                if (progress > 0 && progress < 1)
+                {
+                    thisMonthResult.Progress = $"{achivedChore.Count()} / {customerChore.Frequency}";
+                }
+
+                if (progress == 1)
+                {
+                    thisMonthResult.Progress = "OK";
+                }
+                monthResult.Add(thisMonthResult);
+            }
 
             var choreRow = new ChoreRow
             {
-                ChoreName = customerChore.Chore.Title,
-                MonthResult = new Dictionary<int, string>
-                {
-                    { 1, "Done" },
-                    { 2, "Done" },
-                    { 3, "Done" },
-                    { 4, "Done" },
-                    { 5, "Done" },
-                    { 6, "Done" },
-                    { 7, "Done" },
-                    { 8, "Done" },
-                    { 9, "Done" },
-                    { 10, "Done" },
-                    { 11, "Done" },
-                    { 12, "Done" }
-                }
+                MonthResult = monthResult
             };
+
             choreRows.Add(choreRow);
         }
-        var achiveChores = customerChores.Where(x => x.Status == "Klar").ToList();
-        
-        
-            new ChoreRow
-            {
-                ChoreName = "Chore 1",
-                MonthResult = new Dictionary<int, string>
-                {
-                    { 1, "Done" },
-                    { 2, "Done" },
-                    { 3, "Done" },
-                    { 4, "Done" },
-                    { 5, "Done" },
-                    { 6, "Done" },
-                    { 7, "Done" },
-                    { 8, "Done" },
-                    { 9, "Done" },
-                    { 10, "Done" },
-                    { 11, "Done" },
-                    { 12, "Done" }
-                }
 
-            }
-         
+        return new ReportObject
+        {
+            Id = new Random().Next(0,9999999),
+            CustomerInfo = customerInfo,
+            IssuerInfo = issuerInfo,
+            ChoreRows = choreRows
+        };
+
     }
 }
